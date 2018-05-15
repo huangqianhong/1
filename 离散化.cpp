e345867fc837889b3d2c2300a45a6e14b301831f
huangqianhong <1510851768@qq.com>
2018-05-15 22:41:53 +0800

    Add files via upload

---
+++ 离散化.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+#define maxsize 1000
+void main()
+{
+	int len=0,i,j;
+	int b[maxsize]={0};
+	int c[maxsize]={0};
+	char num[maxsize]={0};
+	gets(num);
+	len=strlen(num);
+	for(i=65;i<123;i++)
+	{
+		b[i]=i;
+	
+	}
+	
+	for(i=65;i<123;i++)
+	{	
+		for(j=0;j<len;j++)
+		{
+		
+			if(b[i]==num[j])
+			{
+				
+				c[i]=c[i]+1;
+			}	
+		}
+	}
+	for(i=65;i<122;i++)
+	{
+		if(c[i]!=0)
+		{
+		  printf("%c的个数为:%d\n",i,c[i]);
+		}
+	}
+	
+	
+}
